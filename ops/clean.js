95c0dc0a1ff622e1a91f260333be74dea19712d0
John Law <johnlaw.po@gmail.com>
2020-03-24 23:33:13 +0100

    Add a cleaning option

--- ops/clean.js
+++ ops/clean.js
@@ -2,47 +2,45 @@ const fs = require('fs')
 const path = require('path')
 const readline = require('readline')
 
-
-async function runClean(){
-    const qAndA = (q) => {
-        const r = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        })
-        return new Promise(resolve => r.question(q, a => {
-            r.close()
-            resolve(a)
-        }))
-    }
-    let confirm = 'N'
-    confirm = await qAndA('Are you sure to clean all the previous dump and output? This action is irreversible.[y/N] ')
-    if(confirm === 'Y' || confirm === 'y'){
-fs.readdir('./out/dump', (err, files) => {
-    if (err) throw err
-    const dateFormateRegex = /\d{4}-\d{2}-\d{2}-\d{4}(-raw)?\.txt/
-    files.forEach(file => {
-        const dir = path.join('./', file)
-        if (file.match(dateFormateRegex)){
-            fs.unlinkSync('./out/dump/' + dir)
-        }
+async function runClean () {
+  const qAndA = (q) => {
+    const r = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
     })
-})
-
-fs.readdir('./out/json', (err, files) => {
-    if (err) throw err
-    const dateFormateRegex = /\d{4}-\d{2}-\d{2}-\d{4}(-raw)?\.json/
-    files.forEach(file => {
+    return new Promise(resolve => r.question(q, a => {
+      r.close()
+      resolve(a)
+    }))
+  }
+  let confirm = 'N'
+  confirm = await qAndA('Are you sure to clean all the previous dump and output? This action is irreversible.[y/N] ')
+  if (confirm === 'Y' || confirm === 'y') {
+    fs.readdir('./out/dump', (err, files) => {
+      if (err) throw err
+      const dateFormateRegex = /\d{4}-\d{2}-\d{2}-\d{4}(-raw)?\.txt/
+      files.forEach(file => {
         const dir = path.join('./', file)
-        if (file.match(dateFormateRegex)){
-            fs.unlinkSync('./out/json/' +dir)
+        if (file.match(dateFormateRegex)) {
+          fs.unlinkSync('./out/dump/' + dir)
         }
+      })
     })
-    console.log('Congrats! Your dump and output files are erased.')
-})
-    }
-    else{
-        console.log('Clean exited due to invalid confirmation.')
-    }
+
+    fs.readdir('./out/json', (err, files) => {
+      if (err) throw err
+      const dateFormateRegex = /\d{4}-\d{2}-\d{2}-\d{4}(-raw)?\.json/
+      files.forEach(file => {
+        const dir = path.join('./', file)
+        if (file.match(dateFormateRegex)) {
+          fs.unlinkSync('./out/json/' + dir)
+        }
+      })
+      console.log('Congrats! Your dump and output files are erased.')
+    })
+  } else {
+    console.log('Clean exited due to invalid confirmation.')
+  }
 }
 
 runClean()

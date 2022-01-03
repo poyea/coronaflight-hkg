f8afa5dceb4737ca6aa258fb38fbdbcd85b7b290
John Law <johnlaw.po@gmail.com>
2022-01-04 00:13:03 +0800

    add gists locations

---
+++ ops/gists.js
@@ -0,0 +1,5 @@
+console.log(`
+Latest\n\thttps://gist.githubusercontent.com/poyea/8ce06b31763379e2084cb2022b88b79a/raw/
+
+History\n\thttps://gist.githubusercontent.com/poyea/fa1f2988c4f424d3e42b9284e751663f/raw/
+`)

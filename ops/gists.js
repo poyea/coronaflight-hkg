ec0986bfa81941e7eb2137c4e91bfa307a638dbc
John Law <johnlaw.po@gmail.com>
2022-01-04 22:47:57 +0800

    add gists command

--- ops/gists.js
+++ ops/gists.js
@@ -1,5 +1,3 @@
-console.log(`
-Latest\n\thttps://gist.githubusercontent.com/poyea/8ce06b31763379e2084cb2022b88b79a/raw/
-
-History\n\thttps://gist.githubusercontent.com/poyea/fa1f2988c4f424d3e42b9284e751663f/raw/
+console.log(`Latest\thttps://gist.githubusercontent.com/poyea/8ce06b31763379e2084cb2022b88b79a/raw/
+History\thttps://gist.githubusercontent.com/poyea/fa1f2988c4f424d3e42b9284e751663f/raw/
 `)
